d69c0d106763238c71181f8a90fb389ceb6618a4
Salman Mahmud <salman_mahmud@hotmail.com>
2023-03-04 15:11:00 -0500

    Revert " testing out new spacing for mobile nav"
    This reverts commit f7655e6e053c9b2e8dafe8fcd86e649fd5170193.

--- features/common/modules/Navigation/components/Mobile/Mobile.js
+++ features/common/modules/Navigation/components/Mobile/Mobile.js
@@ -6,18 +6,18 @@ import { MessageIcon } from '../../icons/Message'
 import { PersonIcon } from '../../icons/Person'
 
 function Mobile() {
-  const size="5rem"
+  const size="75%"
   return (
     <>
-    <nav className='sticky p-1 mx-auto mt-5 font-bold rounded-full shadow-sm w-fit w bottom-10 shadow-slate-400 h-15 md:hidden bg-slate-600'>
+    <nav className='sticky w-1/2 p-1 mx-auto mt-5 font-bold rounded-full shadow-sm bottom-10 shadow-slate-400 h-15 md:hidden bg-slate-600'>
 
 
-          <ul className='flex items-center justify-center flex-1 p-1 m-1 text-blue-300'>
-            <li><HomeIcon height={size} width={size} className='mx-2' /></li>
-            <li><PersonIcon height={size} width={size} className='mx-2' /></li>
-            <li><Laptop height={size} width={size} className='mx-2'  /></li>
-            <li><SkillList height={size} width={size} className='mx-2' /></li>
-            <li><MessageIcon height={size} width={size} className='mx-2'  /></li>
+          <ul className='flex items-center justify-center flex-1 p-1 text-blue-300'>
+            <li><HomeIcon height={size} width={size} className='m-auto' /></li>
+            <li><PersonIcon height={size} width={size} className='m-auto' /></li>
+            <li><Laptop height={size} width={size} className='m-auto'  /></li>
+            <li><SkillList height={size} width={size} className='m-auto' /></li>
+            <li><MessageIcon height={size} width={size} className='m-auto'  /></li>
 
           </ul>
 
