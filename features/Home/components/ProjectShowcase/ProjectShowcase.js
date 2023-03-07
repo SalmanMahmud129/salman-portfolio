47fe8a7001e756661b104eb245268f5908f50fe1
Salman Mahmud <salman_mahmud@hotmail.com>
2023-03-06 22:34:31 -0500

    added consts for projects

--- features/Home/components/ProjectShowcase/ProjectShowcase.js
+++ features/Home/components/ProjectShowcase/ProjectShowcase.js
@@ -8,16 +8,29 @@ import { motion } from 'framer-motion'
 function ProjectShowcase() {
   return (
     <section name='projects' className='w-full h-11/12 bg-blue-300/10' >
-      <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} className=' max-w-screen-full w-full h-full flex flex-col items-center justify-center'>
+      <div  className=' max-w-screen-xl px-5 py-10 mx-auto flex flex-col justify-center w-full h-full'>
         
-        <div className='py-8'>
-          <p className='xs:text-2xl md:text-4xl font-medium inline border-b-4 border-blue-400'>
+        <motion.div initial={{ opacity: 0, y: -50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 2 }} viewport={{once:true}} className="xs:pt-10 md:pt-0 ">
+          <p className='xs:text-xl md:text-4xl font-medium inline border-b-4 border-blue-400'>
             What I Have Worked On
           </p>
+        </motion.div>
+
+        <div className='flex items-center justify-center my-10'>
+
+
+          <div className='flex '>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+          <ProjectCard/>
+
+          </div>
         </div>
 
-      </motion.div>
-      <ProjectCard/>
+
+      </div>
+      
     </section>
   )
 }
---
+++ features/Home/components/ProjectShowcase/components/ProjectCard/projectConsts.js
@@ -0,0 +1,20 @@
+export default projects = [
+    {
+        name: "Flatiron Dealership",
+        image: './CarDealer.png',
+        description: 'My Capstone project for Flatiron School. A mock E-commerce dealership. React front-end and Rails back-end',
+        github: "https://github.com/SalmanMahmud129/Phase-5-Car-Dealer"
+    },
+    {
+        name: "Real Estate Next",
+        image: './RealEstate.png',
+        description: 'My introduction to Next.js and Chakra ui. Mock front-end for a real estate company with data pull from Bayut Api',
+        github: "https://github.com/SalmanMahmud129/real-estate-next"
+    },
+    {
+        name: "My Portfolio",
+        image: "",
+        description: "The website you are on currently!",
+        github: "https://github.com/SalmanMahmud129/salman-portfolio"
+    }
+]
