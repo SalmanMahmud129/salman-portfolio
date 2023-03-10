ee9ffdbe5dd0a950a95671d67a227e0e3abb5259
Salman Mahmud <salman_mahmud@hotmail.com>
2023-03-09 23:31:25 -0500

    skills consts added

--- features/Home/components/SkillsSection/skillConsts.js
+++ features/Home/components/SkillsSection/skillConsts.js
@@ -3,6 +3,95 @@ import { LogosReact } from "@/features/common/Icons/ReactIcon"
 import { LogosNextjsIcon } from "@/features/common/Icons/NextjsIcon"
 import { LogosRuby } from "@/features/common/Icons/RubyIcon"
 import { LogosRails } from "@/features/common/Icons/RubyIcon"
+import { LogosPostgresql } from "@/features/common/Icons/PostgresIcon"
+import { LogosSqlite } from "@/features/common/Icons/SQLIcon"
+import { LogosHtml5 } from "@/features/common/Icons/HTMLIcon"
+import { LogosCss3 } from "@/features/common/Icons/CSSIcon"
+import { LogosSemanticUi } from "@/features/common/Icons/SemanticUiIcon"
+import { ChakraIcon } from "@/features/common/Icons/ChakraIcon"
+import { LogosTailwindcssIcon } from "@/features/common/Icons/TailwindIcon"
+import { GitHubIcon } from "@/features/common/Icons/GithubIcon"
+import { LogosPostmanIcon } from "@/features/common/Icons/PostmanIcon"
+import { LogosStripe } from "@/features/common/Icons/StripeIcon"
+
+
 export const skills = [
+    {
+        id: 1,
+        skill: "Javascript",
+        icon: LogosJavascript
+
+    },
+    {
+        id: 2,
+        skill: "React.js",
+        icon: LogosReact
+
+    },
+    {
+        id: 3,
+        skill: "Next.js",
+        icon: LogosNextjsIcon
+    },
+    {
+        id: 4,
+        skill: "Ruby",
+        icon: LogosRuby
+    },
+    {
+        id: 5,
+        skill: "Ruby on Rails",
+        icon: LogosRails
+    },
+    {
+        id: 6,
+        skill: "PostgreSQL",
+        icon: LogosPostgresql
+    },
+    {
+        id: 7,
+        skill: "SQLite",
+        icon: LogosSqlite
+    },
+    {
+        id: 8,
+        skill: "HTML",
+        icon: LogosHtml5
+    },
+    {
+        id: 9,
+        skill: "CSS",
+        icon: LogosCss3
+    },
+    {
+        id: 10,
+        skill: "Semantic-Ui",
+        icon: LogosSemanticUi
+    },
+    {
+        id: 11,
+        skill: "Chakra-Ui",
+        icon: ChakraIcon
+    },
+    {
+        id: 12,
+        skill: "Tailwind CSS",
+        icon: LogosTailwindcssIcon
+    },
+    {
+        id: 13,
+        skill: "Git/Github",
+        icon: GitHubIcon
+    },
+    {
+        id: 14,
+        skill: "Postman",
+        icon: LogosPostmanIcon
+    },
+    {
+        id: 15,
+        skill: "Stripe",
+        icon: LogosStripe
+    }
 
 ]
